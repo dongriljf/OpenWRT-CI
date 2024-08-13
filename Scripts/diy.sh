d5b0e989a2378aa86ca6c26004dd0dd6dacfab37
dongriljf <41283005+dongriljf@users.noreply.github.com>
2024-08-13 20:48:56 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -4,8 +4,8 @@ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 #sed -i '$a src-git ikoolproxy   https://github.com/ilxp/luci-app-ikoolproxy' feeds.conf.default
 ./scripts/feeds update -a 
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-rm -rf feeds/smpackage/{*openclash,*homeproxy,*ikoolproxy}
-./scripts/feeds install wrtbwmon
+#rm -rf feeds/smpackage/{*openclash,*homeproxy,*ikoolproxy}
+#./scripts/feeds install wrtbwmon
 
 #rm -rf feeds/luci/applications/luci-app-openclash
 #cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
