c9c0d56372535f0bfb5f02d3a20fa1ea05796a82
dongriljf <41283005+dongriljf@users.noreply.github.com>
2024-08-17 00:47:41 +0800

    t

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -58,12 +58,18 @@ fi
 PW_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall/Makefile")
 if [ -f "$PW_FILE" ]; then
 	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/x86_64/d' $PW_FILE
-	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW_FILE
-	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $PW_FILE
+	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks /,/default n/d' $PW_FILE
+	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d' $PW_FILE
 
 	cd $PKG_PATCH && echo "passwall has been fixed!"
 fi
-
+#if [ -f "$PW_FILE" ]; then
+#	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/x86_64/d' $PW_FILE
+#	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW_FILE
+#	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $PW_FILE
+#
+#	cd $PKG_PATCH && echo "passwall has been fixed!"
+#fi
 
 #SP_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-ssr-plus/Makefile")
 #if [ -f "$SP_FILE" ]; then
--- Scripts/diy_lean.sh
+++ Scripts/diy_lean.sh
@@ -3,5 +3,5 @@ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 
 ./scripts/feeds update -a 
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-
+rm -rf feeds/smpackage/{*serverchan,*ikoolproxy}
 ./scripts/feeds install -a  
