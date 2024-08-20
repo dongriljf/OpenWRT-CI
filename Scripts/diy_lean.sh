dc74eabfc0d6889f52da1ff58a1fe50a8ee5eef4
dongriljf <41283005+dongriljf@users.noreply.github.com>
2024-08-21 01:16:22 +0800

    一样

--- Scripts/diy_lean.sh
+++ Scripts/diy_lean.sh
@@ -10,26 +10,42 @@ if [[ $WRT_REPO == *"padavanonly/immortalwrt"* ]]; then
             cp -f  $GITHUB_WORKSPACE/e8820s/mt7621_d-team_newifi-d2.dts target/linux/ramips/dts/mt7621_d-team_newifi-d2.dts
             cp -f  $GITHUB_WORKSPACE/e8820s/mt7621_zte_e8820s.dts       target/linux/ramips/dts/mt7621_zte_e8820s.dts 
             cp -f  $GITHUB_WORKSPACE/e8820s/platform.sh                 target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
-   else
-        echo "   ZTE_e8820s need to be installed!"
+	   else
+        echo "   ZTE_e8820s  be installed!"
    fi
-fi
-
-
+			
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 ./scripts/feeds update -a
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-git clone -b main --single-branch https://github.com/asvow/luci-app-tailscale  package/luci-app-tailscale
+#git clone -b main --single-branch https://github.com/asvow/luci-app-tailscale  package/luci-app-tailscale
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-rm -rf feeds/packages/net/{alist,adguardhome,smartdns,mosdns,xray*,v2ray*,sing*}
-rm -rf feeds/packages/net/{tailscale,brook,chinadns-ng,dns2socks,dns2tcp,gn,hysteria,ipt2socks,microsocks,naiveproxy}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns,*openclash,*homeproxy,*serverchan,*ikoolproxy}
+rm -rf feeds/packages/net/{brook,chinadns-ng,dns2socks,dns2tcp,gn,hysteria,ipt2socks,microsocks,naiveproxy}
 rm -rf feeds/packages/net/{redsocks2,shadow-tls,shadowsocks-rust,shadowsocksr-libev,simple-obfs,ssocks,tcping,trojan*,tuic-client}
-rm -rf feeds/luci/applications/{luci-app-bypass,luci-app-homeproxy,luci-app-mosdns,luci-app-passwall*,luci-app-ssr-plus}
+rm -rf feeds/luci/applications/{luci-app-bypass,luci-app-homeproxy,luci-app-mosdns,luci-app-passwall*,luci-app-ssr-plus,luci-app-mosdns}
 #cp -rf feeds/smpackage/{luci-app-bypass,luci-app-homeproxy,luci-app-mosdns,luci-app-passwall,luci-app-ssr-plus}  feeds/luci/applications/
 rm -rf feeds/packages/utils/{v2dat,lua-neturl}
 #./scripts/feeds install tailscale 
 #./scripts/feeds install v2ray-geodata 
 #./scripts/feeds install v2dat 
 ./scripts/feeds install -a 
+else
+
+
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+./scripts/feeds update -a
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+rm -rf feeds/luci/applications/{luci-app-bypass,luci-app-homeproxy,luci-app-mosdns,luci-app-passwall*,luci-app-ssr-plus,luci-app-mosdns}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns,*openclash,*homeproxy,*serverchan,*ikoolproxy}
+rm -rf feeds/packages/utils/v2dat
+./scripts/feeds install wrtbwmon
+./scripts/feeds install -a 
+
+
+fi
+
+
