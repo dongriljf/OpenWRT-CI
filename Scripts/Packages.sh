4cfffb313eb462f703eb6a798a924ffe30848219
dongriljf <41283005+dongriljf@users.noreply.github.com>
2024-09-07 00:21:05 +0800

    阿打发

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -25,7 +25,7 @@ UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "$([[ $WRT_REPO == *"lede"* 
 UPDATE_PACKAGE "design" "0x676e67/luci-theme-design" "$([[ $WRT_REPO == *"lede"* ]] && echo "main" || echo "js")"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "$([[ $WRT_REPO == *"lede"* ]] && echo "main" || echo "js")"
 
-UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
+#UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 #UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
 #UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main"
 #UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
