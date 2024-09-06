b9376f5688a0eedec6f830317420e2ab8b5f8c3f
dongriljf <41283005+dongriljf@users.noreply.github.com>
2024-09-06 21:30:37 +0800

    yes

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -60,22 +60,32 @@ if [ -f "$PW_FILE" ]; then
 	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/x86_64/d' $PW_FILE
 	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks /,/default n/d' $PW_FILE
 	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d' $PW_FILE
+	
+	#sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW_FILE
+	#sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $PW_FILE
 
 	cd $PKG_PATCH && echo "passwall has been fixed!"
 fi
-#if [ -f "$PW_FILE" ]; then
-#	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/x86_64/d' $PW_FILE
-#	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW_FILE
-#	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $PW_FILE
-#
-#	cd $PKG_PATCH && echo "passwall has been fixed!"
-#fi
 
-#SP_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-ssr-plus/Makefile")
-#if [ -f "$SP_FILE" ]; then
-#	sed -i '/default PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/libev/d' $SP_FILE
-#	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/x86_64/d' $SP_FILE
-#	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $SP_FILE
-#
-#	cd $PKG_PATCH && echo "ssr-plus has been fixed!"
-#fi
+PW2_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-passwall2/Makefile")
+if [ -f "$PW2_FILE" ]; then
+	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/x86_64/d' $PW2_FILE
+	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks /,/default n/d' $PW2_FILE
+	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d' $PW2_FILE
+	
+	#sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/default n/d' $PW2_FILE
+	#sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $PW2_FILE
+
+	cd $PKG_PATCH && echo "passwall2 has been fixed!"
+fi
+
+SP_FILE=$(find ./ -maxdepth 3 -type f -wholename "*/luci-app-ssr-plus/Makefile")
+if [ -f "$SP_FILE" ]; then
+	sed -i '/default PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks_Libev/,/libev/d' $SP_FILE
+	sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_Shadowsocks /,/x86_64/d' $SP_FILE
+	sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d' $SP_FILE
+
+	#sed -i '/config PACKAGE_$(PKG_NAME)_INCLUDE_ShadowsocksR/,/x86_64/d' $SP_FILE
+	#sed -i '/Shadowsocks_NONE/d; /Shadowsocks_Libev/d; /ShadowsocksR/d' $SP_FILE
+	cd $PKG_PATCH && echo "ssr-plus has been fixed!"
+fi
